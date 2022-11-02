0b5597baeaa04e97de246103cc1962999d5d1af9
doctorpack <enricopachera@gmail.com>
2022-11-02 16:45:08 +0100

    end exercise

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const eleGrid = document.querySelector('.grid');
 
-for (let i = 0; i < 64; i++) {
+for (let i = 1; i <= 100; i++) {
 	// eleGrid.innerHTML += '<div class="cell"></div>';
 
 	const eleCell = document.createElement('div');
 	eleCell.classList.add('cell');
 	eleGrid.append(eleCell);
+    eleCell.append([i]);
 
 	eleCell.addEventListener('click', function () {
 		this.classList.toggle('active')
